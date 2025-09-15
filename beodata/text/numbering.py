77d832fdf3b05dccb90e4fe60f3137243b3c9cf4
Callie Tweney <cat@pobox.com>
2025-09-15 16:25:58 -0700

    Adjust some comments around fitt numbering

--- beodata/text/numbering.py
+++ beodata/text/numbering.py
@@ -11,7 +11,8 @@ from typing import Final, List, Tuple
 # Fitt boundaries: (start_line, end_line, fitt_name)
 # Note: Fitt 24 doesn't exist in Beowulf, but is included for easier calculations
 # The Roman numerals are from the heorot.dk numbering system which follows the
-# MS's.
+# manuscripts faithfully (including the strange Roman numeral convention and the
+# missing fitt and lines)
 FITT_BOUNDARIES: Final[List[Tuple[int, int, str]]] = [
     (1, 52, "Prologue"),  # prologue
     (53, 114, "I"),  # 1
@@ -35,11 +36,11 @@ FITT_BOUNDARIES: Final[List[Tuple[int, int, str]]] = [
     (1251, 1320, "XVIIII"),
     (1321, 1382, "XX"),  # 20
     (1383, 1472, "XXI"),
-    (1473, 1556, "XXII"),  # 22 -- there is no 24
+    (1473, 1556, "XXII"),
     (1557, 1650, "XXIII"),
     (0, 0, "XXIIII"),  # 24 is not real, but having it in the array makes calcs easier
-    (1651, 1739, "XXV"),
-    (1740, 1816, "XXVI"),  # 26
+    (1651, 1739, "XXV"),  # 25
+    (1740, 1816, "XXVI"),
     (1817, 1887, "XXVII"),
     (1888, 1962, "XXVIII"),
     (1963, 2038, "XXVIIII"),
@@ -60,7 +61,7 @@ FITT_BOUNDARIES: Final[List[Tuple[int, int, str]]] = [
 ]
 
 
-# Commented out code for reference - this was used to generate the LINE_NUMBER_MARKERS
+# Commented out code for reference - this was used to generate the FITT_BOUNDARIES
 # number_markers = {}
 # offset = 0
 # for i in range(0, 3182):
@@ -78,5 +79,3 @@ FITT_BOUNDARIES: Final[List[Tuple[int, int, str]]] = [
 #         offset = -2
 #     if i > 0 and i % 5 == 0:
 #         number_markers[i + offset] = i + offset
-#
-# print(number_markers)
